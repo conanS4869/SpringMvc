2fc6c567b378b02151139ac85c860cb4ad68c8a1
conanS4869 <87404816@qq.com>
2020-07-21 17:30:51 +0800

    1

--- springmvc-04-controller/src/main/java/com/conan/controller/ModelTest1.java
+++ springmvc-04-controller/src/main/java/com/conan/controller/ModelTest1.java
@@ -1,19 +1,19 @@
 package com.conan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class ModelTest1 {
     @RequestMapping("/m1/t1")
-    public String test(HttpServletRequest request, HttpServletResponse response) {
-        HttpSession session = request.getSession();
-        System.out.println(session.getId());
+    public String test(Model model) {
+        model.addAttribute("msg", "ModelTest1");
+//转发
+//        return "/WEB-INF/jsp/test.jsp";
+//        return "forward:/WEB-INF/jsp/test.jsp";
 
-        return "test";
+        //重定向
+        return "redirect:/index.jsp";
     }
 }
---
+++ springmvc-04-controller/src/main/java/com/conan/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.conan.controller;
+
+import com.conan.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/t1")
+    public String test1(@RequestParam("name") String name, Model model) {
+        model.addAttribute("msg", name);
+        return "test";
+    }
+
+    @GetMapping("/t2")
+    public String test2(User user) {
+        System.out.println(user);
+        return "test";
+    }
+}
